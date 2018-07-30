e22934811779f3ba5da75b69bf004fdf9a918468
ElGringov2 <ElGringov2@users.noreply.github.com>
2018-07-30 22:34:57 +0200

    Create DBPilot.php

---
+++ DBPilot.php
@@ -0,0 +1,31 @@
+<?php
+
+class Upgrade
+{
+    public $cannon; //int
+    public $modification; //int
+    public $sensor; //int
+    public $talent; //int
+    public $torpedo; //int
+    public $crew; //int?
+    public $gunner; //int?
+    public $force; //int?
+    public $turret; //int?
+    public $device; //int?
+    public $missile; //int?
+    public $illicit; //int?
+}
+
+class Pilot
+{
+    public $name; //String
+    public $ship; //String
+    public $cost; //int
+    public $xws; //String
+    public $unique; //boolean
+    public $upgrade; //Upgrade
+}
+
+
+
+?>
