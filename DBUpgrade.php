be2bca8d8622a4b3d89640b6993f817c93999939
ElGringov2 <ElGringov2@users.noreply.github.com>
2018-07-30 22:34:00 +0200

    Create DBUpgrade.php

---
+++ DBUpgrade.php
@@ -0,0 +1,22 @@
+<?php
+class UpgradeCost
+{
+    public $basecost; //int
+    public $costagi2; //int
+    public $costagi3; //int
+    public $costagi4; //int
+    public $costmed; //int
+    public $costlarge; //int
+}
+
+class Upgrade
+{
+    public $faction; //String
+    public $unique; //boolean
+    public $slots; //int
+    public $type; //String
+    public $xws; //String
+    public $name; //String
+    public $cost; //Cost
+}
+?>
